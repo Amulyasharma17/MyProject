b6f96387095846287a5788e38962eaa4b75cc527
Amulyasharma17 <85661240+Amulyasharma17@users.noreply.github.com>
2021-06-10 21:53:31 +0530

    Add files via upload

---
+++ course.js
@@ -0,0 +1,16 @@
+function test(){
+var x = document.getElementById("course").value
+
+if(x == 'java')
+{
+console.log("you have selected java")
+}
+
+else if(x == 'angular'){
+console.log("you have selected angular")
+}
+else{
+console.log("you have selected python")
+}
+}
+
